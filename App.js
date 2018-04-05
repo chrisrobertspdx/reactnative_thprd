f7a3ad4a03894efd67b6ac6424e192267cfb1b09
Francisco Chacon <pacochacon94@gmail.com>
2018-04-05 12:14:04 -0500

    Remove unnecessary imports from App.js

--- App.js
+++ App.js
@@ -4,8 +4,6 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './screens';
 
