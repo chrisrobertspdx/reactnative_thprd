caa1d7b4a7c22eb447ec08641e5474c020b47fb8
Francisco Chacon <pacochacon94@gmail.com>
2018-04-05 12:13:28 -0500

    Start app using new component

--- screens/index.js
+++ screens/index.js
@@ -1,6 +1,8 @@
 import { Navigation } from 'react-native-navigation';
 import SecondScreen from '../src/SecondScreen';
+import Welcome from '../src/Welcome';
 
 export function registerScreens() {
+  Navigation.registerComponent('navigation.Welcome', () => Welcome);
   Navigation.registerComponent('navigation.SecondScreen', () => SecondScreen);
 }
